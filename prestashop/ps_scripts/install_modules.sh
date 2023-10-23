db1b58bd49cdbc7681cf0f1e38f385cd6c27925b
Adam Rak <raqsasg@gmail.com>
2023-10-23 09:54:38 +0200

    Fix installing from downloaded zip files
    Usually zip archive will be module_name-1.7-0.2.zip not module_name.zip
    
    The real module name should be taken from the directory inside

--- prestashop/ps_scripts/install_modules.sh
+++ prestashop/ps_scripts/install_modules.sh
@@ -6,8 +6,15 @@ set -e
 INSTALL_COMMAND="${PS_DIR}/bin/console prestashop:module --no-interaction install"
 MODULES_TO_INSTALL=/ps-modules
 
+get_module_name() {
+  local zip_file="$1"
+  local module_name=$(unzip -l "$zip_file" | awk 'NR==4{print $4}' | xargs basename)
+
+  echo "$module_name"
+}
+
 for file in $(ls "${MODULES_TO_INSTALL}"/*.zip); do
-  module=$(basename ${file} .zip | tr "-" "\n" | head -n 1)
+  module=$(get_module_name $file)
   echo "--> installing ${module} from ${file}...";
   rm -rf "${MODULE_DIR}/${module:-something-at-least}"
   unzip -qq ${file} -d ${MODULE_DIR}
